ed57352682fd6b455cf8d048de32663202a29de5
Enmanuel Almonte <ealmonte@c2r7s4.42malaga.com>
2025-01-07 17:42:27 +0100

    until pipex

---
+++ pipex/src/pipex.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ealmonte <ealmonte@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 19:25:36 by ealmonte          #+#    #+#             */
+/*   Updated: 2025/01/02 19:25:39 by ealmonte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include<sys/types.h>
+# include<sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+int		open_file(char *file, int n);
+char	*my_getenv(char *name, char **env);
+char	*get_path(char *cmd, char **env);
+void	exec(char *cmd, char **env);
+void	ft_free_tab(char **tab);
+void	exit_handler(int n_exit);
+
+#endif
---
+++ pipex/src_bonus/pipex_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ealmonte <ealmonte@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 19:24:47 by ealmonte          #+#    #+#             */
+/*   Updated: 2025/01/02 19:24:49 by ealmonte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include<sys/types.h>
+# include<sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+void	exit_handler(int n_exit);
+int		open_file(char *file, int n);
+char	*my_getenv(char *name, char **env);
+char	*get_path(char *cmd, char **env);
+void	exec(char *cmd, char **env);
+void	ft_free_tab(char **tab);
+
+#endif
